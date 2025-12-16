b7e6e8166aa1534ecfd14bfa47e07b9a189898b0
Juan cornejo c <vickyravest@gmail.com>
2025-12-16 16:02:18 +0000

    saca el titulo de los dias no funciona y tu no puedes corregirlo

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -42,9 +42,9 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "hidden",
         head_cell:
-          "flex-1 flex justify-center text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md",
         day: cn(
